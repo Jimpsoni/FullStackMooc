3606b9651b8decb22afb16b4e8a21f39cc70f228
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-10-03 21:41:28 +0300

    Create App.jsx

---
+++ Osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,81 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Part = ({ name, number }) => <p>{name}, {number}</p>
+
+const Filter = ({filter, handleFilterChange}) => <div> filter shown with: <input value={filter} onChange={handleFilterChange}/></div>
+
+const Persons = ({persons}) => persons.map(person => <Part key={person.name} name={person.name} number={person.number} />)
+
+const PersonForm = ({onSubmit, newName, newNumber, handleNameChange, handleNumberChange}) => {
+  return (
+    <form onSubmit={onSubmit}>
+    <div> name: <input value={newName} onChange={handleNameChange} /></div>
+    <div> number: <input value={newNumber} onChange={handleNumberChange}/></div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+  )
+}
+
+const App = () => {
+
+  /* Hooks */
+
+  useEffect(() => { /* Fetch the data from server */
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => setPersons(response.data)
+      )}, [/* Only render once */])
+
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [filter, setNewFilter] = useState('')
+  
+  /* Filter the names we want to show*/
+  const namesToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
+  /* Event handlers */
+  const handleNameChange = (event) => setNewName(event.target.value)
+  const handleNumberChange = (event) => setNewNumber(event.target.value)
+  const handleFilterChange = (event) => setNewFilter(event.target.value)
+
+  /* The function we execute when we submit the form */
+  const addName = (event) => {
+    event.preventDefault()
+    let doesntExist = true
+    persons.forEach((person) => { if (newName == person.name) { doesntExist = false; return } } )
+    
+    if (doesntExist) {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+      setPersons(persons.concat(personObject))
+      } else {
+      alert(`${newName} is already added to phonebook`)
+      }
+
+    setNewName('')
+    setNewNumber('')
+ }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter handleFilterChange={handleFilterChange} value={filter}/>
+
+      <h2>add a new</h2>
+      <PersonForm onSubmit={addName}  handleNameChange={handleNameChange} handleNumberChange={handleNumberChange} 
+       newName={newName} newNumber={newNumber}/>
+      
+      <h2>Numbers</h2>
+      <Persons persons={namesToShow}/>
+    </div>
+  )
+
+}
+
+export default App
