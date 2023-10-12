d6c8278151135b14395e3f3410a5859e41621b30
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-10-12 21:23:14 +0300

    Update index.js

--- Osa3/puhelinluettelo backend/index.js
+++ Osa3/puhelinluettelo backend/index.js
@@ -1,15 +1,22 @@
+// Connect to DB
+const Person = require('./models/person')
+
 // määritellään express
 const express = require('express')
-const morgan = require('morgan')
 const app = express()
+
+require('dotenv').config()
+
+app.use(express.static('dist'))
 const cors = require('cors')
 
 // Middlewaret
 app.use(express.json())
 app.use(cors())
 
-morgan.token('data', function (req, res) { if (req.method == "POST") {return JSON.stringify(req.body)} })
 
+const morgan = require('morgan')
+morgan.token('data', function (req, res) { if (req.method == "POST") {return JSON.stringify(req.body)} })
 app.use(
     morgan(function (tokens, req, res) {
         return [
@@ -25,82 +32,96 @@ app.use(
 )
 
 
-// Määritellään yhteystiedot
-let persons = [
-      {
-        "name": "Arto Hellas",
-        "number": "321",
-        "id": 1
-      },
-      {
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523",
-        "id": 2
-      },
-      {
-        "name": "Dan Abramov",
-        "number": "12-43-234345",
-        "id": 3
-      },
-      {
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122",
-        "id": 4
-      },
-      {
-        "name": "Peppi",
-        "number": "123",
-        "id": 5
-      }
-]
-
-
-// funktio joka luo uuden ID:n
-const generateID = () => Math.floor(Math.random() * 1000)
-
-
 // Määritellään API kutsut
-app.get('/api/persons', (req, res) => { res.json(persons) })
-
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id) // Etsitään ID
-
-    if(person == undefined) {res.status(404).end()}
-    else { res.json(person) }
+app.get('/api/persons', (req, res, next) => { 
+  Person.find({}).then(person => {
+  res.json(person) })
+  .catch(error => next(error))
 })
 
-app.get('/info', (req, res) => {
-    const message = `Phonebook has info for ${persons.length} people`
-    const time = new Date();
-    console.log(time)
-    res.send(`<p> ${message}</p> <p> ${time}</p>`)
+
+app.get('/api/persons/:id', (req, res, next) => {
+    const id = req.params.id
+
+    Person.findById(id)
+      .then( result => res.status(200).send(result))
+      .catch( error => next(error) )
+
+})
+
+
+app.get('/info', (req, res, next) => {
+    Person.countDocuments({})
+    .then(
+      result => {
+        const message = `Phonebook has info for ${result} people`
+        const time = new Date();
+        res.send(`<p> ${message}</p> <p> ${time}</p>`)
+      }
+    )
+    .catch( error => next(error))
 }) 
 
-app.delete('/api/persons/:id', (req, res) => {
-    const id = req.params.id
-    persons = persons.filter(person => person.id != id)
-    res.status(204).end()
+
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+      .then( result => res.status(204).end() )
+      .catch( error => next(error) )
 })
 
-app.post("/api/persons", (req, res) => {
-    const body = req.body
-    const nameExists = persons.find(person => person.name == body.name)
-    
-    if (nameExists) return res.json({"error": "Person with that name already exists"})
-    if (!body.name || !body.number) return res.json({"error": "missing data, check that you have name and number"})
 
-    const newPerson = {
+app.put('/api/persons/:id', (req, res, next) => {
+  const body = req.body
+
+  const person = {
+    'name': body.name,
+    'number': body.number
+  }
+
+  Person.findByIdAndUpdate(body.id, person, { 
+    new: true,
+    runValidators: true, 
+    context: 'query'
+  })
+    .then(updatedPerson => res.json(updatedPerson))
+    .catch(error => next(error))
+})
+
+
+app.post("/api/persons", (req, res, next) => {
+    const body = req.body
+    
+    const newPerson = new Person({
         "name": body.name,
         "number": body.number,
-        "id": generateID()
-    }
+    })
 
     // Lisätään uusi henkilö yhteystietoihin
-    persons = persons.concat(newPerson)
-    res.status(200).end()
+    newPerson.save().then(person => {
+      res.json(person)
+    })
+    .catch(error => next(error))
 })
 
+
+// Error handler middleware
+const errorHandler = (error, request, response, next) => {
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  if (error.name === 'ServerError') {
+    return response.status(500).send({error: 'Internal Server error'})
+  }
+
+  if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 // Käynnistetään serveri
 const PORT = process.env.PORT || 3001
