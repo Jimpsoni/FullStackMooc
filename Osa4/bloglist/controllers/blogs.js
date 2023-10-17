63fe1fe8abfb6eb46196627a200d5205f11443c0
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-10-17 03:01:39 +0300

    Add files via upload

--- Osa4/bloglist/controllers/blogs.js
+++ Osa4/bloglist/controllers/blogs.js
@@ -1,5 +1,7 @@
 const blogRouter = require('express').Router()
+const { response } = require('../app')
 const Blog = require("../models/blogs")
+
 
 blogRouter.get('/', (request, response) => {
     Blog
@@ -9,15 +11,34 @@ blogRouter.get('/', (request, response) => {
       })
   })
   
+
 blogRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
-    console.log(request.body)
     blog
       .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
+      .then(result => { response.status(201).json(result) })
+      .catch(error => { if (error.name === 'ValidationError') response.status(400).json({ error: error.message }) })
+      
   })
 
+
+blogRouter.delete('/:id', (request, response) => {
+  Blog.findByIdAndRemove(request.params.id)
+  .then( () => response.status(204).end() )
+  .catch( () => response.status(400).end() )
+})
+
+
+blogRouter.put('/:id', (request, response) => {
+  const likes  = request.body.likes
+  
+  Blog.findByIdAndUpdate(request.params.id, {likes: likes}, { 
+    new: false,
+    runValidators: true,
+    context: 'query'
+  })
+  .then( updatedBlog => response.json(updatedBlog) )
+  .catch( () => response.status(400).end() )
+})
 
 module.exports = blogRouter
