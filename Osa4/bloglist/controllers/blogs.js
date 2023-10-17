9e0efbb4a745e1c4328a7a9426227824d78de0c5
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-10-18 00:15:21 +0300

    Add files via upload

--- Osa4/bloglist/controllers/blogs.js
+++ Osa4/bloglist/controllers/blogs.js
@@ -1,31 +1,53 @@
 const blogRouter = require('express').Router()
 const { response } = require('../app')
 const Blog = require("../models/blogs")
+const userExtractor = require('../utils/middleware').userExtractor
 
 
 blogRouter.get('/', (request, response) => {
     Blog
       .find({})
+      .populate('user')
       .then(blogs => {
         response.json(blogs)
       })
   })
   
 
-blogRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-    blog
-      .save()
-      .then(result => { response.status(201).json(result) })
-      .catch(error => { if (error.name === 'ValidationError') response.status(400).json({ error: error.message }) })
-      
+blogRouter.post('/', userExtractor, async (request, response) => {
+  const body = request.body
+  const user = request.user
+
+  const newBlog = {
+    ...body,
+    user: user.id
+  }
+  
+  const blog = new Blog(newBlog)
+  blog
+    .save()
+    .then(result => { response.status(201).json(result) })
+    .catch(error => { if (error.name === 'ValidationError') response.status(400).json({ error: error.message }) })
   })
 
 
-blogRouter.delete('/:id', (request, response) => {
-  Blog.findByIdAndRemove(request.params.id)
-  .then( () => response.status(204).end() )
-  .catch( () => response.status(400).end() )
+blogRouter.delete('/:id', userExtractor, async (request, response) => {
+  let blog
+  try {
+    blog = await Blog.findById(request.params.id)
+  } 
+  catch { return response.status(400).end() }
+
+  if ( blog.user.toString() === request.user.id.toString() ) {
+    Blog.findByIdAndRemove(request.params.id)
+    .then( () => response.status(204).end() )
+    .catch( () => response.status(500).end() )
+  } else {
+    response.status(401).end()
+  }
+
+
+
 })
 
 
---
+++ Osa4/bloglist/controllers/users.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/users')
+
+
+// add new user
+usersRouter.post('/', async (req, res) => {
+    if ( req.body.password.length < 3 ) return res.status(400).json({ error: "Bad password, be must atleast 3 chars" })
+    const { username, name, password } = req.body
+
+
+    // Hashing and salting
+    const saltRounds = 10
+    const passwordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User({
+        username,
+        name,
+        passwordHash
+    })
+
+    user.save()
+    .then(savedUser => res.status(201).json(savedUser))
+    .catch( error => { if (error.name === "ValidationError") res.status(400).json( { error: "Bad username, must be atleast 3 chars and unique" } )} )
+})
+
+
+// Show users
+usersRouter.get('/', (req, res) => {
+    User.find({})
+    .populate('blogs')
+    .then( response => res.json(response) )
+
+})
+
+module.exports = usersRouter
